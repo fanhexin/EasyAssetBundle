6a130c3f172f443ad8c1d83fde353f58a4887666
fanhexin <fanhexin@gmail.com>
2019-08-12 11:01:24 +0800

    1. 添加AssetBundle缓存功能 2. 添加设置窗口

--- Config.cs
+++ Config.cs
@@ -15,7 +15,19 @@ namespace EasyAssetBundle
             Real
         }
         
-        public Mode mode => (Mode)EditorPrefs.GetInt(MODE_SAVE_KEY, 0);
+        public Mode mode
+        {
+            get => (Mode) EditorPrefs.GetInt(MODE_SAVE_KEY, 0);
+            set => EditorPrefs.SetInt(MODE_SAVE_KEY, (int) value);
+        }
+
+        public const string BUILD_OPTIONS_SAVE_KEY = "easy_asset_bundle_build_options";
+        public BuildAssetBundleOptions buildOptions
+        {
+            get => (BuildAssetBundleOptions) EditorPrefs.GetInt(BUILD_OPTIONS_SAVE_KEY,
+                    (int) BuildAssetBundleOptions.ChunkBasedCompression);
+            set => EditorPrefs.SetInt(BUILD_OPTIONS_SAVE_KEY, (int)value);
+        }
 #endif
 
         [SerializeField] string _manifestName = "Manifest";
--- Editor/ConfigInspector.cs
+++ Editor/ConfigInspector.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
@@ -8,6 +9,7 @@ namespace EasyAssetBundle.Editor
     [CustomEditor(typeof(Config))]
     public class ConfigInspector : UnityEditor.Editor
     {
+        const string CACHE_BASE_PATH = "Assets/../Library/EasyAssetBundleCache";
         const string STREAMING_ASSETS = "StreamingAssets";
         Config _target;
         bool _showProcessors;
@@ -21,24 +23,25 @@ namespace EasyAssetBundle.Editor
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
+            
+            EditorGUI.BeginChangeCheck();
             Config.Mode mode = (Config.Mode) EditorGUILayout.EnumPopup("Mode", _target.mode);
             if (mode == Config.Mode.Real && !File.Exists(Path.Combine(Application.streamingAssetsPath, _target.manifestName)))
             {
                 EditorGUILayout.HelpBox("Please build assetbundle first!", MessageType.Error);    
             }
             
-            if (GUI.changed)
+            if (EditorGUI.EndChangeCheck())
             {
-                EditorPrefs.SetInt(Config.MODE_SAVE_KEY, (int) mode);
+                _target.mode = mode;
             }
 
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("Build Settings", EditorStyles.boldLabel);
-            string[] guids = AssetDatabase.FindAssets($"t: {nameof(AbstractBuildProcessor)}");
-            var processors = guids.Select(AssetDatabase.GUIDToAssetPath).Select(AssetDatabase.LoadAssetAtPath<AbstractBuildProcessor>);
-            if (guids.Length > 0)
+            IEnumerable<AbstractBuildProcessor> processors = GetProcessors();
+            if (processors.Any())
             {
-                _showProcessors = EditorGUILayout.Foldout(_showProcessors, $"Build Processors ({guids.Length})");
+                _showProcessors = EditorGUILayout.Foldout(_showProcessors, $"Build Processors ({processors.Count()})");
                 if (_showProcessors)
                 {
                     int indentLevel = EditorGUI.indentLevel;
@@ -53,21 +56,25 @@ namespace EasyAssetBundle.Editor
                 }
             }
             
-            _buildAbOptions = (BuildAssetBundleOptions) EditorGUILayout.EnumFlagsField("Build Options", _buildAbOptions);
+            EditorGUI.BeginChangeCheck();
+            var options = (BuildAssetBundleOptions) EditorGUILayout.EnumFlagsField("Build Options", _target.buildOptions);
+            if (EditorGUI.EndChangeCheck())
+            {
+                _target.buildOptions = options;
+            }
+            
             if (GUILayout.Button("Build Asset Bundle"))
             {
-                foreach (var processor in processors)
-                {
-                    processor.BeforeBuild();
-                }
-                
-                BuildAssetBundle(EditorUserBuildSettings.activeBuildTarget);
-                
-                foreach (var processor in processors)
-                {
-                    processor.AfterBuild();
-                }
+                BuildAssetBundle(_target.manifestName, _buildAbOptions, processors);
             }
+        }
+
+        static IEnumerable<AbstractBuildProcessor> GetProcessors()
+        {
+            string[] guids = AssetDatabase.FindAssets($"t: {nameof(AbstractBuildProcessor)}");
+            var processors = guids.Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<AbstractBuildProcessor>);
+            return processors;
         }
 
         static void ClearFolder(string folderPath)
@@ -85,8 +92,47 @@ namespace EasyAssetBundle.Editor
             }
         }
 
-        void BuildAssetBundle(BuildTarget buildTarget, params AssetBundleBuild[] buildMap)
+        public static void BuildAssetBundle()
         {
+            var config = Resources.Load<Config>("EasyAssetBundleConfig");
+            BuildAssetBundle(config.manifestName, config.buildOptions, GetProcessors());
+        }
+
+        static void BuildAssetBundle(string manifestName, BuildAssetBundleOptions buildOptions, IEnumerable<AbstractBuildProcessor> processors)
+        {
+            foreach (var processor in processors)
+            {
+                processor.BeforeBuild();
+            }
+            
+            BuildAssetBundle(EditorUserBuildSettings.activeBuildTarget, buildOptions, manifestName);
+            
+            foreach (var processor in processors)
+            {
+                processor.AfterBuild();
+            }
+        }
+
+        static void BuildAssetBundle(BuildTarget buildTarget,
+            BuildAssetBundleOptions buildOptions = BuildAssetBundleOptions.ChunkBasedCompression,
+            string manifestName = "Manifest",
+            params AssetBundleBuild[] buildMap)
+        {
+            string cachePath = Path.Combine(CACHE_BASE_PATH, buildTarget.ToString());
+            if (!Directory.Exists(cachePath))
+            {
+                Directory.CreateDirectory(cachePath);
+            }
+            
+            if (buildMap != null && buildMap.Length != 0)
+            {
+                BuildPipeline.BuildAssetBundles(cachePath, buildMap, buildOptions, buildTarget);
+            }
+            else
+            {
+                BuildPipeline.BuildAssetBundles(cachePath, buildOptions, buildTarget);
+            }
+            
             string destPath = Application.streamingAssetsPath;
             if (Directory.Exists(destPath))
             {
@@ -97,18 +143,49 @@ namespace EasyAssetBundle.Editor
                 AssetDatabase.CreateFolder("Assets", STREAMING_ASSETS);
             }
 
-            if (buildMap != null && buildMap.Length != 0)
-            {
-                BuildPipeline.BuildAssetBundles(destPath, buildMap, _buildAbOptions, buildTarget);
-            }
-            else
-            {
-                BuildPipeline.BuildAssetBundles(destPath, _buildAbOptions, buildTarget);
-            }
-            
-            File.Move($"{destPath}/{STREAMING_ASSETS}", $"{destPath}/{_target.manifestName}");
-            File.Move($"{destPath}/{STREAMING_ASSETS}.manifest", $"{destPath}/{_target.manifestName}.manifest");
+            DirectoryCopy(cachePath, destPath, true);
+
+            File.Move($"{destPath}/{buildTarget}", $"{destPath}/{manifestName}");
+            File.Move($"{destPath}/{buildTarget}.manifest", $"{destPath}/{manifestName}.manifest");
             AssetDatabase.Refresh();
+        }
+
+        static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+        
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
         }
     }
 }
