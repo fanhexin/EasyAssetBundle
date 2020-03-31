70be9beacddcbeb79740858813902f0910e09873
fanhexin <fanhexin@gmail.com>
2020-03-31 19:23:55 +0800

    添加对单个bundle包含多个文件的支持

--- Editor/BundleExtension.cs
+++ Editor/BundleExtension.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using UnityEditor;
 using Object = UnityEngine.Object;
@@ -6,15 +7,9 @@ namespace EasyAssetBundle.Editor
 {
     public static class BundleExtension
     {
-        public static string Guid(this Object obj)
-        {
-            string path = AssetDatabase.GetAssetPath(obj);
-            return AssetDatabase.AssetPathToGUID(path);
-        }
-        
         public static bool Any(this SerializedProperty bundles, string abName)
         {
-            for (int i = 0; i < bundles.arraySize - 1; i++)
+            for (int i = 0; i < bundles.arraySize; i++)
             {
                 if (bundles.GetArrayElementAtIndex(i)
                     .FindPropertyRelative("_name").stringValue == abName)
@@ -37,6 +32,14 @@ namespace EasyAssetBundle.Editor
             if (string.IsNullOrEmpty(abName))
             {
                 abName = Path.GetFileNameWithoutExtension(assetPath).ToLower();
+                if (bundles.Any(abName))
+                {
+                    abName += $"_conflict_{DateTime.Now.ToBinary()}";
+                }
+                
+                var importer = AssetImporter.GetAtPath(assetPath);
+                importer.assetBundleName = abName;
+                importer.SaveAndReimport();
             }
             else
             {
@@ -46,22 +49,35 @@ namespace EasyAssetBundle.Editor
                 }
             }
 
-            string guid = AssetDatabase.AssetPathToGUID(assetPath);
-            if (bundles.Any(abName))
-            {
-                abName += $"_conflict_{guid}";
-            }
-            
-            var importer = AssetImporter.GetAtPath(assetPath);
-            importer.assetBundleName = abName;
-            importer.SaveAndReimport();
+            return bundles.AddBundleByAbName(abName);
+        }
 
+        public static string AddBundleByAbName(this SerializedProperty bundles, string abName)
+        {
+            string guid = Guid.NewGuid().ToString();
             var newItem = bundles.GetArrayElementAtIndex(bundles.arraySize++);
             newItem.FindPropertyRelative("_guid").stringValue = guid;
             newItem.FindPropertyRelative("_name").stringValue = abName;
             newItem.FindPropertyRelative("_type").enumValueIndex = 0;
             bundles.serializedObject.ApplyModifiedProperties();
             return guid;
+        }
+
+        public static string AddBundle(this SerializedProperty bundles, string abName, params string[] assetPaths)
+        {
+            foreach (string path in assetPaths)
+            {
+                var importer = AssetImporter.GetAtPath(path);
+                string oldName = importer.assetBundleName;
+                importer.assetBundleName = abName;
+                importer.SaveAndReimport();
+                if (!string.IsNullOrEmpty(oldName))
+                {
+                    AssetDatabase.RemoveAssetBundleName(oldName, false);
+                }
+            }
+
+            return bundles.AddBundleByAbName(abName);
         }
     }
 }
