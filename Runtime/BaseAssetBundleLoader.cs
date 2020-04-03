301c153290262ca1ce7b112e66006a5a87990ec1
fanhexin <fanhexin@gmail.com>
2020-04-03 16:40:18 +0800

    重构AssetBundleLoader，减少冗余代码

---
+++ Runtime/BaseAssetBundleLoader.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading;
+using EasyAssetBundle.Common;
+using UniRx.Async;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace EasyAssetBundle
+{
+    public abstract class BaseAssetBundleLoader : IAssetBundleLoader
+    {
+        protected readonly RuntimeSettings _runtimeSettings;
+
+        protected BaseAssetBundleLoader(RuntimeSettings runtimeSettings)
+        {
+            _runtimeSettings = runtimeSettings;
+        }
+
+        private string GuidToName(string guid)
+        {
+            return _runtimeSettings.guid2BundleDic[guid].name;
+        }
+        
+        public abstract UniTask<IAssetBundle> LoadAsync(string name, IProgress<float> progress = null, CancellationToken token = default);
+
+        public async UniTask<(IAssetBundle ab, T asset)> LoadAssetAsync<T>(string abName, string assetName, IProgress<float> progress = null,
+            CancellationToken token = default) where T : Object
+        {
+            using (var handler = ProgressDispatcher.instance.Create(progress))
+            {
+                IProgress<float> loadAssetProgress = handler.CreateProgress();
+                var ab = await LoadAsync(abName, handler.CreateProgress(), token);
+                var asset = await ab.LoadAssetAsync<T>(assetName, loadAssetProgress, token);
+                return (ab, asset);
+            }        
+        }
+
+        public UniTask<IAssetBundle> LoadByGuidAsync(string guid, IProgress<float> progress = null, CancellationToken token = default)
+        {
+            return LoadAsync(GuidToName(guid), progress, token);
+        }
+
+        public UniTask<(IAssetBundle ab, T asset)> LoadAssetByGuidAsync<T>(string guid, string assetName, IProgress<float> progress = null,
+            CancellationToken token = default) where T : Object
+        {
+            return LoadAssetByGuidAsync<T>(GuidToName(guid), assetName, progress, token);
+        }
+
+        public abstract Hash128? GetCachedVersionRecently(string abName);
+    }
+}
--- Runtime/VirtualAssetBundleLoader.cs
+++ Runtime/VirtualAssetBundleLoader.cs
@@ -4,47 +4,23 @@ using System.Threading;
 using EasyAssetBundle.Common;
 using UniRx.Async;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace EasyAssetBundle
 {
-    internal class VirtualAssetBundleLoader : IAssetBundleLoader
+    internal class VirtualAssetBundleLoader : BaseAssetBundleLoader
     {
-        private readonly RuntimeSettings _runtimeSettings;
-
         public VirtualAssetBundleLoader(RuntimeSettings runtimeSettings)
+            : base(runtimeSettings)
         {
-            _runtimeSettings = runtimeSettings;
         }
         
-        public async UniTask<IAssetBundle> LoadAsync(string name, IProgress<float> progress, CancellationToken token)
+        public override async UniTask<IAssetBundle> LoadAsync(string name, IProgress<float> progress, CancellationToken token)
         {
             progress?.Report(1);
             return new VirtualAssetBundle(name);    
         }
 
-        public async UniTask<(IAssetBundle ab, T asset)> LoadAssetAsync<T>(string abName, string assetName,
-            IProgress<float> progress = null, CancellationToken token = default) where T : Object
-        {
-            var ab = new VirtualAssetBundle(abName);
-            var asset = await ab.LoadAssetAsync<T>(assetName, progress, token);
-            return (ab, asset);
-        }
-
-        public UniTask<IAssetBundle> LoadByGuidAsync(string guid, IProgress<float> progress, CancellationToken token)
-        {
-            string name = _runtimeSettings.guid2BundleDic[guid].name;
-            return LoadAsync(name, progress, token);
-        }
-
-        public UniTask<(IAssetBundle ab, T asset)> LoadAssetByGuidAsync<T>(string guid, string assetName,
-            IProgress<float> progress = null, CancellationToken token = default) where T : Object
-        {
-            string name = _runtimeSettings.guid2BundleDic[guid].name;
-            return LoadAssetAsync<T>(name, assetName, progress, token);
-        }
-
-        public Hash128? GetCachedVersionRecently(string abName)
+        public override Hash128? GetCachedVersionRecently(string abName)
         {
             return new Hash128();
         }
