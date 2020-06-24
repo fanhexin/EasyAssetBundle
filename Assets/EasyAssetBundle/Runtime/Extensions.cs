72b29dbbebdd52c95c68596b10186503b54109e8
fanhexin <fanhexin@gmail.com>
2020-06-24 09:49:43 +0800

    解决编译问题

--- Assets/EasyAssetBundle/Runtime/Extensions.cs
+++ Assets/EasyAssetBundle/Runtime/Extensions.cs
@@ -1,8 +1,11 @@
 #if UNITY_EDITOR
+using EasyAssetBundle.Common.Editor;
 using UnityEditor;
 #endif
 using System;
+using System.IO;
 using System.Threading;
+using EasyAssetBundle.Common;
 using UniRx.Async;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -45,5 +48,62 @@ namespace EasyAssetBundle
             progress?.Report(1);
             return operation.webRequest;
         }
-    }
+
+//         static string GetRemoteBaseUrl(this RuntimeSettings runtimeSettings)
+//         {
+// #if UNITY_EDITOR
+//             string platformName = Application.platform.ToGenericName();
+//             if (Settings.instance.httpServiceSettings.enabled)
+//             {
+//                 return $"{Settings.instance.simulateUrl}/{platformName}";
+//             }
+//
+//             return string.IsNullOrEmpty(runtimeSettings.cdnUrl) ? string.Empty : $"{runtimeSettings.cdnUrl}/{platformName}";
+// #else
+//             return string.IsNullOrEmpty(runtimeSettings.cdnUrl) ? string.Empty : $"{runtimeSettings.cdnUrl}/{platformName}";
+// #endif
+//         }
+//
+//         public static string GetLocalUrl(string name)
+//         {
+//             string basePath;
+// #if UNITY_EDITOR
+//             basePath = Settings.currentTargetCachePath;
+// #else
+//             basePath = Config.streamingAssetsBundlePath;
+// #endif
+//             
+//             string path = Path.Combine(basePath, name);
+// #if UNITY_EDITOR || UNITY_IOS
+//             path = $"file://{path}";
+// #endif
+//             return path;
+//         }
+//
+//         public static string GetRemoteUrl(this RuntimeSettings runtimeSettings, string name)
+//         {
+//             string baseUrl = runtimeSettings.GetRemoteBaseUrl();
+//             string url = $"{baseUrl}/{name}";
+//             
+// #if UNITY_EDITOR
+//             // 编辑器模式下如果没有启用HTTPService或者设置cdn url，就直接从本地cache中加载
+//             if (string.IsNullOrEmpty(baseUrl))
+//             {
+//                 return GetLocalUrl(name);
+//             }
+//             
+//             if (Settings.instance.httpServiceSettings.enabled)
+//             {
+//                 return url;
+//             }
+// #endif
+//             
+//             if (!string.IsNullOrEmpty(runtimeSettings.cdnUrl) &&
+//                 runtimeSettings.webRequestProcessor != null)
+//             {
+//                 url = runtimeSettings.webRequestProcessor.HandleUrl(url);
+//             }
+//             return url;
+//         }
+     }
 }
